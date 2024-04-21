c5c77ea5ce2519577fd650efae8e76c5acb6f22c
My Name <i@yuzukihd.top>
2024-04-21 16:04:03 +0800

    update

--- build.sh
+++ build.sh
@@ -5,4 +5,12 @@ sudo apt install -y qemu-user-static
 docker build -t mesa-jammy .
 docker run -itd --name mesa-jammy mesa-jammy:latest
 docker cp mesa-jammy:/workspace .
-cp workspace/*.deb .
+mkdir mesa-repo
+cp workspace/*.deb mesa-repo
+
+git clone https://github.com/AvaotaSBC/mesa.git -b jammy-devel mesa-repo-tmp
+cp mesa-repo-tmp/.git mesa-repo
+cd mesa-repo
+git add .
+git add -f .github
+git commit -m "${DATE} mesa-pkg update"
